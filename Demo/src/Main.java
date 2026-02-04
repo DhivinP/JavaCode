0efad736ca438795d11956c6d3280a2abfe10483
DhivinP <pandidhivin@gmail.com>
2026-02-04 08:24:31 +0530

    feat : method overriding

--- Demo/src/Main.java
+++ Demo/src/Main.java
@@ -1,7 +1,20 @@
+class A{
+    public void welcome(){
+        System.out.println("Welcome to Java Course");
+    }
+        }
+
+class B extends A{
+    public void welcome(){
+        System.out.println("Welcome to Java and springboot Course");
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("New java course");
+
+        A obj = new B();
+        obj.welcome();
     }
 }
 
